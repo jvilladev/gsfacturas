2552bf1a928895c024abd63a501fae00532298d2
Jhoniel Villalaz <jvillalaz@tuprimeravivienda.com>
2024-09-25 16:39:34 -0500

    modulo inicial login

---
+++ resources/views/main.blade.php
@@ -0,0 +1,62 @@
+<!-- 
+* Copyright 2016 Carlos Eduardo Alfaro Orellana
+-->
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Login</title>
+	
+
+    <link rel="stylesheet" href="{{ asset('css/normalize.css') }}">
+    <!-- <link rel="stylesheet" href="css/normalize.css"> -->
+
+
+	<!-- <link rel="stylesheet" href="css/sweetalert2.css"> -->
+	<link rel="stylesheet" href="{{ asset('css/sweetalert2.css') }}">
+
+	<link rel="stylesheet" href="{{ asset('css/material.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/material-design-iconic-font.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/jquery.mCustomScrollbar.css') }}">
+	
+	<link rel="stylesheet" href="{{ asset('css/main.css') }}">
+	
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="asset(\'js/jquery-1.11.2.min.js\')"><\/script>')</script>
+	
+	<!-- <script src="js/sweetalert2.min.js" ></script> -->
+	<script src="{{ asset('js/sweetalert2.min.js') }}" ></script>
+    
+	
+	
+	<script src="{{ asset('js/material.min.js') }}" ></script>
+	
+	<script src="{{ asset('js/jquery.mCustomScrollbar.concat.min.js') }}" ></script>
+	<script src="{{ asset('js/main.js')}} " ></script>
+
+
+
+</head>
+<body class="cover">
+	<div class="container-login">
+		<p class="text-center" style="font-size: 80px;">
+			<i class="zmdi zmdi-account-circle"></i>
+		</p>
+		<p class="text-center text-condensedLight">Sign in with your Account</p>
+		<form>
+			<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+			    <input class="mdl-textfield__input" type="text" id="userName">
+			    <label class="mdl-textfield__label" for="userName">User Name</label>
+			</div>
+			<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+			    <input class="mdl-textfield__input" type="password" id="pass">
+			    <label class="mdl-textfield__label" for="pass">Password</label>
+			</div>
+			<button id="SingIn" class="mdl-button mdl-js-button mdl-js-ripple-effect" style="color: #3F51B5; float:right;">
+				Sign in <i class="zmdi zmdi-mail-send"></i>
+			</button>
+		</form>
+	</div>
+</body>
+</html>
